e2d9dc378021b8e7ad019de057a829c50e0c78b5
galew <lew.garrett@gmail.com>
2014-02-27 17:20:20 -0800

    Update sched.pl

--- sched.pl
+++ sched.pl
@@ -1,34 +1,34 @@
 /* all course names */
 c_numbers(N) :-
-	course(N,_,_).
+        course(Name,_,_).
  
 /* numbers of all computer programming language courses */
-c_pl(N) :- 
-	course(N, programming_languages,_). 
+c_pl(N) :-
+        course(Name,programming_languages,_).
  
 /* numbers of all non-computer programming language courses */
 c_notpl(N) :-
-        course(Name, notpl, _).
+        course(Name,not(programming_languages),_).
  
 /* list of professors teaching 60 */
 c_inst60(L) :-
-        course(60,pl,L).
+        course(60,_,L).
  
 /* sorted list of professors teaching 60 */
 c_inst60_sorted(L) :-
-        sort(60,pl,L).
+        sort(course(60,_,L)).
  
 /* list of professors teaching 20 */
 c_inst20(L) :-
-        course(20,pl,L).
+        course(20,_,L).
  
 /* sorted list of professors teaching 20 */
 c_inst20_sorted(L) :-
-        sort(20,pl,L).
+        sort(course(20,_,L)).
  
 /* sorted list of professors teaching N */
 c_inst_sorted(N,L) :-
-        sort(N,pl,L).
+        sort(course(N,_,L)).
  
 /* number of courses with exactly one instructor */
 c_single_inst(N) :-
@@ -39,8 +39,8 @@ c_multi_inst(N)  :-
     course(N,_,[_|[_|_]]).
  
 /* numbers of courses for which I is the only instructor */
-c_exclusive(I,N) :-
- 
+ c_exclusive(I,N) :- 
+    course(N,_,I).
  
 /* numbers of courses with exactly one or exactly two instructors */
 
@@ -49,10 +49,10 @@ c_exclusive(I,N) :-
 
 /* part 2 */
  
+delete_question("string_answer").
 
 
 
-sortappend(List1, List2, AppendList) :-
-append(List1, List2, Appendlist).
-sort(Appendlist).
-
+sortappend(List1,List2,AppendList) :-
+	append(List1,List2,Appendlist).
+	sort(Appendlist).
