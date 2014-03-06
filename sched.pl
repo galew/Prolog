28ec9f68a73b2edc3706df74db8da647ef58589e
Corina Putinar <cputinar@gmail.com>
2014-03-06 11:26:09 -0800

    Part 1 Tested.

--- sched.pl
+++ sched.pl
@@ -1,34 +1,38 @@
 /* all course names */
 c_numbers(N) :-
-        course(Name,_,_).
+        course(N,_,_).
  
 /* numbers of all computer programming language courses */
 c_pl(N) :-
-        course(Name,programming_languages,_).
+        course(N,programming_languages,_).
  
 /* numbers of all non-computer programming language courses */
 c_notpl(N) :-
-        course(Name,not(programming_languages),_).
+        course(N,ID,_),
+	ID \= 'programming_languages'.
  
 /* list of professors teaching 60 */
 c_inst60(L) :-
         course(60,_,L).
  
 /* sorted list of professors teaching 60 */
-c_inst60_sorted(L) :-
-        sort(course(60,_,L)).
+c_inst60_sorted(OUT) :-
+        course(60,_,L),
+	sort(L, OUT).
  
 /* list of professors teaching 20 */
 c_inst20(L) :-
         course(20,_,L).
  
 /* sorted list of professors teaching 20 */
-c_inst20_sorted(L) :-
-        sort(course(20,_,L)).
+c_inst20_sorted(OUT) :-
+        course(20,_,L),
+	sort(L, OUT).
  
 /* sorted list of professors teaching N */
-c_inst_sorted(N,L) :-
-        sort(course(N,_,L)).
+c_inst_sorted(N,OUT) :-
+        course(N,_,L),
+	sort(L, OUT).
  
 /* number of courses with exactly one instructor */
 c_single_inst(N) :-
@@ -40,15 +44,17 @@ c_multi_inst(N)  :-
  
 /* numbers of courses for which I is the only instructor */
  c_exclusive(I,N) :- 
-    course(N,_,I).
+    course(N,_,[I|[]]).
  
 /* numbers of courses with exactly one or exactly two instructors */
 c_12_inst_1or(N):-
-		';'(course(N, _, [_]), course(N, _, [_,_])).
+		(course(N, _, [_]) ; course(N, _, [_,_])).
 
 c_12_inst_2wo(N):-
-		course(N, _, Q),
-		Q \= [_,_,_].
+		course(N, _, [_ | []]).
+
+c_12_inst_2wo(N):-
+		course(N, _, [_ | [_ | []]]).
 
 
 /* part 2 */
@@ -58,7 +64,7 @@ delete_question("string_answer").
 /* This follows the delete behavior on page 157 */
 
 sortappend([List1],[List2],SortAppendList) :-
-	append([List1],[List2],[Appendlist]).
+	append([List1],[List2],[Appendlist]),
 	sort([Appendlist],SortAppendList).
 
 /* part 3 */
@@ -66,7 +72,7 @@ sortappend([List1],[List2],SortAppendList) :-
 distribute(W,[H],Y) :-
     append(W,H,Y).
 distribute(W,[H|T],Y) :-
-    distribute[W,T,Y1],
+    distribute(W,T,Y1),
     append(Y,Y1,Y).
 
 /* part 4 */
@@ -74,28 +80,27 @@ distribute(W,[H|T],Y) :-
 
 /* myfor(L,U,Result) */
 
+% myfor(L,U,Result) :-
+%    L =< U,
+%    L1 is L+1,
+%    myfor(L1,U,Res1),
+%    Result = [L | Res1].
 
-myfor(L,U,Result) :-
-    L =< U,
-    L1 is L+1,
-    myfor(L1,U,Res1),
-    Result = [L | Res1].
-
-myfor(L,U,[]) :-
-    L>U.
+%myfor(L,U,[]) :-
+%    L>U.
 
 
-crossmyfor(R,H,Z) :- 
-    myfor(1,R,A),
-    myfor(1,H,B).
+%crossmyfor(R,H,Z) :- 
+%    myfor(1,R,A),
+%    myfor(1,H,B).
   
 
-rule1([],_,[]).
+%rule1([],_,[]).
 
-rule1([A|D],B,C) :-
-    distribute(A,B,Result),
-    rule1(D,B,Result2),
-    append(Result,Result2,C).
+%rule1([A|D],B,C) :-
+%    distribute(A,B,Result),
+%    rule1(D,B,Result2),
+%    append(Result,Result2,C).
 
 
 
@@ -110,14 +115,14 @@ rule1([A|D],B,C) :-
 
 
 /* part 5a */
-getallmeetings(C,Z) :- 
-    Z=sort(C);
+% getallmeetings(C,Z) :- 
+%    Z=sort(C);
 
 /* part 5b */ 
-participants(C,Z) :-
+% participants(C,Z).
 
 /* part 5c */
-osched(MR,MH,C,Z) :-
+% osched(MR,MH,C,Z).
 
 /* part 5d */
-xsched(MR, MH, C, Z) :-
+% xsched(MR, MH, C, Z).
